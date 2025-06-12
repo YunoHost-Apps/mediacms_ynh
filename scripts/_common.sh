73bedb0827e87e1eeb58df1906b01cf7dee6cd07
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2025-06-12 22:24:08 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,14 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+install_ffmpeg () {
+# install ffmpeg
+echo "Downloading and installing ffmpeg"
+wget -q https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+mkdir -p tmp
+tar -xf ffmpeg-release-amd64-static.tar.xz --strip-components 1 -C tmp
+cp -v tmp/{ffmpeg,ffprobe,qt-faststart} /usr/local/bin
+rm -rf tmp ffmpeg-release-amd64-static.tar.xz
+echo "ffmpeg installed to /usr/local/bin"
+}
+
